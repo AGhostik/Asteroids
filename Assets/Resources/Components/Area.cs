4ae57dc3cfd2280ac7899757efdadb0d0db5b94f
Aidar Yakupov <yakupov.aidar.ru@yandex.ru>
2019-04-27 14:07:28 +0500

    add Spawner; add Area

---
+++ Assets/Resources/Components/Area.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Resources.Components {
+    public class Area {
+        public Area(Vector2 leftUpPoint, Vector2 rightDownPoint) {
+            _leftUpPoint = leftUpPoint;
+            _rightDownPoint = rightDownPoint;
+
+            _leftDownPoint = new Vector2(leftUpPoint.x, rightDownPoint.y);
+            _rightUpPoint = new Vector2(rightDownPoint.x, leftUpPoint.y);
+        }
+
+        private readonly Vector2 _leftUpPoint;
+        private readonly Vector2 _rightDownPoint;
+
+        private readonly Vector2 _leftDownPoint;
+        private readonly Vector2 _rightUpPoint;
+
+        public Vector2 GetRandomPoint() {
+            var randomX = Random.Range(_leftUpPoint.x, _rightDownPoint.x);
+            var randomY = Random.Range(_leftUpPoint.y, _rightDownPoint.y);
+            return new Vector2(randomX, randomY);
+        }
+
+        public void DebugDraw(Color color) {
+            Debug.DrawLine(_leftUpPoint, _rightUpPoint, color);
+            Debug.DrawLine(_rightUpPoint, _rightDownPoint, color);
+            Debug.DrawLine(_rightDownPoint, _leftDownPoint, color);
+            Debug.DrawLine(_leftDownPoint, _leftUpPoint, color);
+
+            Debug.DrawLine(_leftUpPoint, _rightDownPoint, color);
+            Debug.DrawLine(_rightUpPoint, _leftDownPoint, color);
+        }
+    }
+}
---
+++ Assets/Resources/Components/Spawner.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Resources.Components {
+    //[System.Serializable]
+    //public class Enemy {
+    //    public GameObject enemy;
+    //}
+
+    public class Spawner : MonoBehaviour {
+        public Camera mainCamera;
+        public float spawnFieldWidth = 3f;
+        public float timeBetweenSpawns = 5f;
+        public int enemyCountLimit = 10;
+        public GameObject enemy;
+
+        private readonly List<GameObject> _enemiesCached = new List<GameObject>();
+        private Area[] _areas;
+        private Transform _enemyContainer;
+        private float _nextSpawnTimeLeft;
+        private Area _visibleArea;
+
+        private void Awake() {
+            _areas = _createZonesBeyondCameraVisibility();
+            _visibleArea = _createVisibleArea();
+
+            var thisTransform = GetComponent<Transform>();
+            var bulletContainer = new GameObject("EnemyContainer");
+            _enemyContainer = bulletContainer.GetComponent<Transform>();
+            _enemyContainer.position = thisTransform.position;
+            _enemyContainer.parent = thisTransform;
+        }
+
+        private void Update() {
+            if (_nextSpawnTimeLeft <= 0 &&
+                _enemiesCached.Count < enemyCountLimit) {
+                _spawn();
+                _nextSpawnTimeLeft = timeBetweenSpawns;
+            } else {
+                _nextSpawnTimeLeft -= Time.deltaTime;
+            }
+
+            #if DEBUG
+            _areas[0].DebugDraw(Color.red);
+            _areas[1].DebugDraw(Color.green);
+            _areas[2].DebugDraw(Color.blue);
+            _areas[3].DebugDraw(Color.yellow);
+
+            _visibleArea.DebugDraw(Color.cyan);
+            #endif
+        }
+        
+        private void _spawn() {
+            var areaIndex = Random.Range(0, _areas.Length);
+            var area = _areas[areaIndex];
+
+            var nextEnemy = _getNextEnemy();
+
+            nextEnemy.transform.position = area.GetRandomPoint();
+            _lookAt2D(nextEnemy.transform, _visibleArea.GetRandomPoint());
+
+            nextEnemy.SetActive(true);
+        }
+
+        private GameObject _getNextEnemy() {
+            var nextEnemy = _enemiesCached.FirstOrDefault(_ => !_.gameObject.activeInHierarchy);
+            if (nextEnemy == null) {
+                nextEnemy = Instantiate(enemy, _enemyContainer);
+                _enemiesCached.Add(nextEnemy);
+            }
+
+            return nextEnemy;
+        }
+
+        private static void _lookAt2D(Transform transform2D, Vector3 point) {
+            // можно сделать методом расширения
+            var dir = point - transform2D.position;
+            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            transform2D.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+        
+        private Area _createVisibleArea() {
+            var cameraLeftUpPoint = mainCamera.ScreenToWorldPoint(new Vector2(0, 0));
+            var cameraRightDownPoint = mainCamera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+            return new Area(cameraLeftUpPoint, cameraRightDownPoint);
+        }
+
+        private Area[] _createZonesBeyondCameraVisibility() {
+            var cameraLeftUpPoint = mainCamera.ScreenToWorldPoint(new Vector2(0, 0));
+            var cameraLeftDownPoint = mainCamera.ScreenToWorldPoint(new Vector2(0, Screen.height));
+            var cameraRightUpPoint = mainCamera.ScreenToWorldPoint(new Vector2(Screen.width, 0));
+            var cameraRightDownPoint = mainCamera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+            var leftArea = new Area(new Vector2(cameraLeftUpPoint.x - spawnFieldWidth, cameraLeftUpPoint.y), cameraLeftDownPoint);
+            var topArea = new Area(new Vector2(cameraLeftUpPoint.x, cameraLeftUpPoint.y - spawnFieldWidth), cameraRightUpPoint);
+            var rightArea = new Area(cameraRightUpPoint, new Vector2(cameraRightDownPoint.x + spawnFieldWidth, cameraRightDownPoint.y));
+            var bottomArea = new Area(cameraLeftDownPoint, new Vector2(cameraRightDownPoint.x, cameraRightDownPoint.y + spawnFieldWidth));
+
+            return new[] {leftArea, topArea, rightArea, bottomArea};
+        }
+    }
+}
