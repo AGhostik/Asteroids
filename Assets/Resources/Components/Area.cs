7a675d4bb82d7b5f48525e5dc51227f2a7ab245c
Aidar Yakupov <yakupov.aidar.ru@yandex.ru>
2019-04-28 17:57:54 +0500

    add CameraCage; add read-only field to Area; change Spawner, Player and Bullet values

--- Assets/Resources/Components/Area.cs
+++ Assets/Resources/Components/Area.cs
@@ -3,33 +3,32 @@
 namespace Resources.Components {
     public class Area {
         public Area(Vector2 leftUpPoint, Vector2 rightDownPoint) {
-            _leftUpPoint = leftUpPoint;
-            _rightDownPoint = rightDownPoint;
+            LeftUpPoint = leftUpPoint;
+            RightDownPoint = rightDownPoint;
 
-            _leftDownPoint = new Vector2(leftUpPoint.x, rightDownPoint.y);
-            _rightUpPoint = new Vector2(rightDownPoint.x, leftUpPoint.y);
+            LeftDownPoint = new Vector2(leftUpPoint.x, rightDownPoint.y);
+            RightUpPoint = new Vector2(rightDownPoint.x, leftUpPoint.y);
         }
 
-        private readonly Vector2 _leftUpPoint;
-        private readonly Vector2 _rightDownPoint;
-
-        private readonly Vector2 _leftDownPoint;
-        private readonly Vector2 _rightUpPoint;
-
-        public Vector2 GetRandomPoint() {
-            var randomX = Random.Range(_leftUpPoint.x, _rightDownPoint.x);
-            var randomY = Random.Range(_leftUpPoint.y, _rightDownPoint.y);
-            return new Vector2(randomX, randomY);
-        }
+        public Vector2 RightDownPoint { get; }
+        public Vector2 LeftDownPoint { get; }
+        public Vector2 RightUpPoint { get; }
+        public Vector2 LeftUpPoint { get; }
 
         public void DebugDraw(Color color) {
-            Debug.DrawLine(_leftUpPoint, _rightUpPoint, color);
-            Debug.DrawLine(_rightUpPoint, _rightDownPoint, color);
-            Debug.DrawLine(_rightDownPoint, _leftDownPoint, color);
-            Debug.DrawLine(_leftDownPoint, _leftUpPoint, color);
+            Debug.DrawLine(LeftUpPoint, RightUpPoint, color);
+            Debug.DrawLine(RightUpPoint, RightDownPoint, color);
+            Debug.DrawLine(RightDownPoint, LeftDownPoint, color);
+            Debug.DrawLine(LeftDownPoint, LeftUpPoint, color);
 
-            Debug.DrawLine(_leftUpPoint, _rightDownPoint, color);
-            Debug.DrawLine(_rightUpPoint, _leftDownPoint, color);
+            Debug.DrawLine(LeftUpPoint, RightDownPoint, color);
+            Debug.DrawLine(RightUpPoint, LeftDownPoint, color);
+        }
+
+        public Vector2 GetRandomPoint() {
+            var randomX = Random.Range(LeftUpPoint.x, RightDownPoint.x);
+            var randomY = Random.Range(LeftUpPoint.y, RightDownPoint.y);
+            return new Vector2(randomX, randomY);
         }
     }
 }
---
+++ Assets/Resources/Components/CameraCage.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Resources.Components {
+    public class CameraCage : MonoBehaviour {
+        public Camera mainCamera;
+        public GameObject player;
+
+        private float _additionalSpace = 0.1f;
+        private Transform _playerTransform;
+        private Rigidbody2D _playeRigidbody2D;
+        private Area _visibleArea;
+
+        private void Awake() {
+            _visibleArea = _createVisibleArea();
+            _playerTransform = player.GetComponent<Transform>();
+            _playeRigidbody2D = player.GetComponent<Rigidbody2D>();
+        }
+        
+        public void FixedUpdate() {
+            var playerPosition = _playerTransform.position;
+
+            if (playerPosition.x < _visibleArea.LeftUpPoint.x) {
+                _playeRigidbody2D.MovePosition(new Vector2(_visibleArea.RightDownPoint.x - _additionalSpace, playerPosition.y));
+            }
+            if (playerPosition.x > _visibleArea.RightDownPoint.x) {
+                _playeRigidbody2D.MovePosition(new Vector2(_visibleArea.LeftUpPoint.x + _additionalSpace, playerPosition.y));
+            }
+            if (playerPosition.y < _visibleArea.LeftUpPoint.y) {
+                _playeRigidbody2D.MovePosition(new Vector2(playerPosition.x, _visibleArea.RightDownPoint.y - _additionalSpace));
+            }
+            if (playerPosition.y > _visibleArea.RightDownPoint.y) {
+                _playeRigidbody2D.MovePosition(new Vector2(playerPosition.x, _visibleArea.LeftUpPoint.y + _additionalSpace));
+            }
+        }
+
+        private Area _createVisibleArea() {
+            var cameraLeftUpPoint = mainCamera.ScreenToWorldPoint(new Vector2(0, 0));
+            var cameraRightDownPoint = mainCamera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+            return new Area(cameraLeftUpPoint, cameraRightDownPoint);
+        }
+    }
+}
