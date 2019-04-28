f72efc301befb970e3e673878dfec00d4e79b656
Aidar Yakupov <yakupov.aidar.ru@yandex.ru>
2019-04-28 18:09:03 +0500

    add RandomSprite; remove Asteroid_Large_[1-4]

---
+++ Assets/Resources/Components/RandomSprite.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Resources.Components {
+    public class RandomSprite : MonoBehaviour {
+        public Sprite[] sprites;
+
+        private SpriteRenderer _spriteRenderer;
+
+        private void Awake() {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _setRandomSprite();
+        }
+
+        private void OnEnable() {
+            _setRandomSprite();
+        }
+
+        private void _setRandomSprite() {
+            if (sprites.Length <= 0) {
+                return;
+            }
+
+            var indes = Random.Range(0, sprites.Length);
+            _spriteRenderer.sprite = sprites[indes];
+        }
+    }
+}
